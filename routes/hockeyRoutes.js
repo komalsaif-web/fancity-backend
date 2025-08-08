5169fdfe5ecaf291045b9e1d5bf3c5a8eddbd7dd
Komal Rafi <komalsaif296@gmail.com>
2025-08-08 08:12:31 -0700

    add

--- routes/hockeyRoutes.js
+++ routes/hockeyRoutes.js
@@ -2,17 +2,7 @@ const express = require("express");
 const router = express.Router();
 const hockeyController = require("../controllers/hockeyController");
 
-// All combined
+// Only one combined endpoint
 router.get("/all", hockeyController.getAllMatches);
-
-// International
-router.get("/international/past", hockeyController.getPastInternational);
-router.get("/international/upcoming", hockeyController.getUpcomingInternational);
-router.get("/international/live", hockeyController.getLiveInternational);
-
-// League
-router.get("/league/past", hockeyController.getPastLeague);
-router.get("/league/upcoming", hockeyController.getUpcomingLeague);
-router.get("/league/live", hockeyController.getLiveLeague);
 
 module.exports = router;
